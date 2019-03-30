bd1f56b680bebde64401a52497b6b233f768dd07
exuberant-minifier-2741 <htsegaw@gmail.com>
2019-03-30 20:43:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 
-function whisper(string){
-  return string.toLowerCase();
+function shout(string){
+  return string.toUpperCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+  return;
+}
+
+function logWhisper(string){
+  console.log(string.tolowerCase());
+  return;
+}
+
+function sayHiToGrandma(string){
+  if(string.tolowerCase() === string){
+    return string;
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string.toUpperCase() === string){
+    return string;
+  }
+}
+
+function sayHiToGrandma(string){
+  var str = "I love you, too."
+  if(string === "I love you, Grandma."){
+    return str;
+  }
 }
