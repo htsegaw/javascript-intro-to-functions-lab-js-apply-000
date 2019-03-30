3776d121d673526989335d79c4d6ca277a1f5496
exuberant-minifier-2741 <htsegaw@gmail.com>
2019-03-30 21:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,36 @@
 function shout(string){
   return string.toUpperCase();
 }
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+  return;
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+  return;
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === string){
+    return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string.toUpperCase() === string){
+    return "YES INDEED!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string === "I love you, Granda."){
+    return "I love you, too";
+  }
+}
+
